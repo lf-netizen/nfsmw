a6d9bd539cbb64296604d1023dc6e14e0d651d38
LF-netizen <lukaszfaruga@student.agh.edu.pl>
2022-10-27 21:27:47 +0200

    driving with kb interrupt

--- drive.py
+++ drive.py
@@ -1,10 +1,10 @@
-from threading import Thread
+from threading import Thread, Event
 from queue import Queue
 
 from fastai.vision.all import *
 import time
 
-from utils import read_angle, angle_diff_norm, preprocess_img, IMG_SIZE_X, IMG_SIZE_Y, read_speed, get_learner
+from utils import read_angle, angle_diff_norm, preprocess_img, IMG_SIZE_X, IMG_SIZE_Y, read_speed, get_learner, save_screen
 from window_capture import WindowCapture
 
 import cv2
@@ -41,13 +41,17 @@ def no_key():
     kb.release('a')
     kb.release('d')
 
-
-def driving_loop(q_pred, wincap):
+def driving_loop(q_pred, wincap, make_preds):
     desired_diff = 0
     accelerate = 1
     old_angle = 0
 
+    make_preds.set()
     while True:
+        if kb.is_pressed('/'):
+            no_key()
+            break
+
         # read current angle
         try:
             screen = wincap.get_screenshot()
@@ -61,13 +65,25 @@ def driving_loop(q_pred, wincap):
             no_key()
             break
         
+        capture_image = np.any([kb.is_pressed(key) for key in ['up', 'down', 'left', 'right']])
+        if capture_image:
+            make_preds.clear()
+            kb_input = ''
+            for key in ['up', 'down', 'left', 'right']:
+                kb_input += str(int(kb.is_pressed(key)))
+            print(kb_input, time.perf_counter())
+            no_key()
+            continue
+        else:
+            make_preds.set()
+
         # get diff if there is any
         if not q_pred.empty():
             pred = q_pred.get()
             if pred is None:
                 no_key()
                 break
-            accelerate, desired_diff, pred_ad = pred
+            accelerate, desired_diff, _, pred_ad = pred
             # desired_angle = (angle + desired_diff) % 30
 
         # calc angle diff
@@ -83,7 +99,7 @@ def driving_loop(q_pred, wincap):
                 desired_diff = 0
             if np.sign(desired_diff)*pred_ad < 0:
                 print('\nchange')
-                desired_diff = -np.sign(desired_diff)
+                desired_diff = 0
 
         # kb inputs
         if desired_diff == 0:
@@ -104,9 +120,7 @@ def driving_loop(q_pred, wincap):
             # else:
                 slow_down()
 
-        if kb.is_pressed('/'):
-            no_key()
-            break
+
         
 def driving_loop_simple(q_pred, wincap):
     desired_diff = 0
@@ -147,12 +161,17 @@ def driving_loop_simple(q_pred, wincap):
             break
 
 THRESHOLD = 1
-def predict(learn, q_pred, wincap):
+def predict(learn, q_pred, wincap, make_preds):
     for i in range(2, 0, -1):
         print(i)
-        time.sleep(1)
 
     while True:
+        make_preds.wait()
+        if kb.is_pressed('/'):
+            break
+        # if capture_image:
+        #     continue
+
         t_start = time.perf_counter()
         try:
             screen = wincap.get_screenshot()
@@ -166,8 +185,10 @@ def predict(learn, q_pred, wincap):
         t_start = time.perf_counter()
         pred = learn.predict((img, torch.tensor(speed/100)))[0]
         t_stop = time.perf_counter()
+
         accelerate = pred[0] + 0.8
         raw_diff = pred[1] * 10
+        pred_ws = np.argmax(pred[3:6])-1
         pred_ad = np.argmax(pred[7:])-1
         
         # REGRESSION
@@ -179,50 +200,43 @@ def predict(learn, q_pred, wincap):
         angle_diff = raw_diff
         # angle_diff = min(raw_diff**2, 8) * np.sign(raw_diff)
         
-        q_pred.put((accelerate, angle_diff, pred_ad))
+        q_pred.put((accelerate, angle_diff, pred_ws, pred_ad))
         print(f"Time: {(t_stop - t_start)*1000:.4f}\n\nAcc: {accelerate:.5f} \nDiff: {raw_diff:.5f} \nKb: {'{:.3f} | {:.3f} | {:.3f}'.format(*pred[7:][::-1])}\n=============================")
-        
-        if kb.is_pressed('/'):
-            break
+
 
 def test_predict(learn, q_pred, wincap):
     for i in range(2, 0, -1):
         print(i)
-        time.sleep(1)
 
     print('2')
     q_pred.put((1, 2, 0))
-    time.sleep(1)
 
     print('-30')
     q_pred.put((1, -30, 0))
-    time.sleep(3)
     
     print('0')
     q_pred.put((1, 0, 0))
-    time.sleep(1)
 
 
     # print('10')
     # q_pred.put((1, 10, 0))
-    # time.sleep(2)
 
     print('-5')
     q_pred.put((1, -5, 0))
-    time.sleep(0.5)
 
     q_pred.put(None)
-    
+
 def main():
     wincap = WindowCapture('Need for Speed™ Most Wanted')
     q_pred = Queue()
+    make_preds = Event()
     # learn = get_learner_('tiny384_70k_allinp_unfrozen')
     # learn = get_learner_('tiny384_70k_allinp_v1')
-    # learn = get_learner('models/tiny384_70k_allinp_v3')
+    learn = get_learner('models/tiny384_70k_allinp_v3')
     # learn = get_learner('models/tiny384_70k_allinp02off_v1')
 
-    t1 = Thread(target=driving_loop, args=(q_pred, wincap))
-    t2 = Thread(target=predict, args=(learn, q_pred, wincap))
+    t1 = Thread(target=driving_loop, args=(q_pred, wincap, make_preds))
+    t2 = Thread(target=predict, args=(learn, q_pred, wincap, make_preds))
 
     t1.start()
     t2.start()
